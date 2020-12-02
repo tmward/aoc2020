0a0c57618001807e2a6879057662ce7c8f36dff9
Thomas M Ward <thomas@thomasward.com>
2020-12-02 10:41:54 -0500

    Solve Day 1 with python

---
+++ day01/solve.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from itertools import combinations
+from functools import reduce
+from operator import mul
+
+with open('input.txt', 'r') as ifile:
+    nums = {int(n) for n in ifile}
+
+print("Part 1's answer:")
+for a, b in combinations(nums, 2):
+    if a + b == 2020:
+        print(a + b)
+        break
+
+print("Part 2's answer:")
+for combo in combinations(nums, 3):
+    if sum(combo) == 2020:
+        print(reduce(mul, combo))
+        break
