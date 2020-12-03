9a2dc170555054aa6b85ed39ccb595faab9cd007
Thomas M Ward <thomas@thomasward.com>
2020-12-03 15:52:39 -0500

    Move initial solution for pt1 into own file
    I generalize this in order to solve pt 1. Keep it here for
    documentation.

---
+++ day03/solve_pt1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from itertools import count, cycle, islice
+
+with open("input.txt", "r") as ifile:
+    print("Part 1 solution:")
+    print(
+        # use True == 1 and False == 0 to our advantage
+        sum(
+            # check if it's a tree
+            map(
+                lambda x: x == "#",
+                # need to use next to get the actual value since islice
+                # returns an iterator
+                map(
+                    next,
+                    # pull the correct spot the sled goes through on
+                    # each line
+                    map(
+                        islice,
+                        # get the lines and make them repeat forever
+                        map(cycle, map(lambda l: l.strip(), ifile)),
+                        count(0, 3),
+                        count(1, 3),
+                    ),
+                ),
+            )
+        )
+    )
