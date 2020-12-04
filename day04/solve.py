fd91f0f23f522c5a6dddcfed47497e84cc52bc8e
Thomas M Ward <thomas@thomasward.com>
2020-12-04 09:28:43 -0500

    Rename pt1 method given new pt2 requirements

--- day04/solve.py
+++ day04/solve.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-from pprint import pprint
 
 
-def is_valid(passport):
+def contains_required_fields(passport):
     required = ("byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:")
-    return all(x in passport for x in required)
+    return all(f in passport for f in required)
 
 
 with open("input.txt", "r") as batch_file:
     passports = batch_file.read().strip().split("\n\n")
 
 print("Part 1 solution:")
-print(sum(is_valid(p) for p in passports))
+print(sum(contains_required_fields(p) for p in passports))
