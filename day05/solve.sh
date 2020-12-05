a36e78c03ebc0b11a4aa201bcba13cd72766753c
Thomas M Ward <thomas@thomasward.com>
2020-12-05 15:47:49 -0500

    Add Day 5 part 2 solution with unix utils
    awk flying to the rescue

--- day05/solve.sh
+++ day05/solve.sh
@@ -5,3 +5,7 @@
 
 echo "Part 1 solution:"
 tr 'FBLR' '0101' < input.txt | sort -r | head -n1 | sed 's/.*/2i&p/' | dc
+
+echo "Part 2 solution:"
+tr 'FBLR' '0101' < input.txt | sed 's/.*/2i&p/' | dc | sort -h |
+    awk 'NR == 1 {prev = $0 - 1} {if (prev != $0 - 1) {print $0 - 1; exit}; prev = $0}'
