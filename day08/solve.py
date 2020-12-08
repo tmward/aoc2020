74f14fe92c3f779c4e73e140dd0ccf37bfbe5218
Thomas M Ward <thomas@thomasward.com>
2020-12-08 09:30:35 -0500

    Make pt 1 solution extensible

--- day08/solve.py
+++ day08/solve.py
@@ -2,25 +2,29 @@
 from pprint import pprint
 
 with open("input.txt", "r") as ifile:
-    instructions = []
+    orig_instructions = []
     for l in ifile:
         op, arg = l.strip().split()
-        instructions.append({'op': op, 'arg': int(arg)})
+        orig_instructions.append({'op': op, 'arg': int(arg)})
 
-accumulator = 0
-seen = [False] * len(instructions)
-i = 0
-while True:
-    if seen[i]:
-        print(f"Pt one solution: {accumulator}")
-        break
-    seen[i] = True
-    op = instructions[i]['op']
-    arg = instructions[i]['arg']
-    if op == 'nop':
-        i += 1
-    elif op == 'acc':
-        accumulator += arg
-        i += 1
-    elif op == 'jmp':
-        i += arg
+def run(instructions):
+    accumulator = 0
+    seen = [False] * len(instructions)
+    i = 0
+    while True:
+        if seen[i]:
+            print("Instruction already seen")
+            return accumulator
+        seen[i] = True
+        op = instructions[i]['op']
+        arg = instructions[i]['arg']
+        if op == 'nop':
+            i += 1
+        elif op == 'acc':
+            accumulator += arg
+            i += 1
+        elif op == 'jmp':
+            i += arg
+
+
+print(f"Part 1 solution: {run(orig_instructions)}")
