60bfde0a84508fe90fcdd7db00a60676067ca099
Thomas M Ward <thomas@thomasward.com>
2020-12-09 10:54:26 -0500

    Add Day 9 pt 2 solution

--- day09/solve.py
+++ day09/solve.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from collections import deque
 from itertools import combinations
+from sys import exit
 
 with open("input.txt", "r") as ifile:
     ns = [int(l) for l in ifile]
@@ -9,6 +10,19 @@ prev_ns = deque(ns[:25])
 
 for n in ns[25:]:
     if all(num1 + num2 != n for num1, num2 in combinations(prev_ns, 2)):
-        print(f"Part 1 solution: {n}")
+        weakness = n
+        break
     prev_ns.popleft()
     prev_ns.append(n)
+
+print(f"Part 1 solution: {weakness}")
+
+# Pt 2 solution
+# algorithm: brute force by obtaining all the possible subslices
+for i in range(len(ns) - 1):
+    seq = ns[i:]
+    for a in range(2, len(seq)):
+        run = seq[:a]
+        if sum(run) == weakness:
+            print(f"Part 2 solution: {min(run) + max(run)}")
+            exit()
