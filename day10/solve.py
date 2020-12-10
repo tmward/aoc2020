ad77980ddbd4f8b4fbd548804b1ccb71b6dc9871
Thomas M Ward <thomas@thomasward.com>
2020-12-10 14:53:52 -0500

    Add pt 2 solution

--- day10/solve.py
+++ day10/solve.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
 from collections import Counter
 
-
 with open("input.txt", "r") as ifile:
     nums = [ int(l) for l in ifile]
 
-nums_sorted = sorted(nums)
+nums_sorted = [0] + sorted(nums)
 # for your adapter that is 3 joules higher
 nums_sorted.append(nums_sorted[-1] + 3)
 
 diffs = []
 for i, n in enumerate(nums_sorted):
     if i == 0:
-        diffs.append(n)
-    else:
-        diffs.append(n - nums_sorted[i - 1])
+        continue
+    diffs.append(n - nums_sorted[i - 1])
 
 diffs_counted = Counter(diffs)
 print(f"Part 1 solution is {diffs_counted.get(1) * diffs_counted.get(3)}")
-#print(f"Part 1 solution is {cum_diff}")
+
+# add previous 
+n1s_to_npaths = [1, 1, 2, 4, 7, 11]
+paths = "".join(str(n) for n in diffs).split("3")
+num_paths = 1
+
+for path in paths:
+    num_paths *= n1s_to_npaths[len(path)]
+
+print(f"Part 2 solution is {num_paths}")
