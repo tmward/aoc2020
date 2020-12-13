7bfd8cd79b8aedb6ef89639c65d8dcb3275c1ed4
Thomas M Ward <thomas@thomasward.com>
2020-12-13 15:18:40 -0500

    Add day 13 pt 2 solution

---
+++ day13/solve_pt2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from itertools import count
+import sys
+
+
+def get_input(filename):
+    with open(filename, "r") as ifile:
+        return [
+            int(t) if t != "x" else 1 for t in ifile.read().splitlines()[1].split(",")
+        ]
+
+
+def solve(buses):
+    for t in count(100000000000000):
+        for offset, bus in enumerate(buses):
+            if (t + offset) % bus != 0:
+                break
+        else:
+            return t
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: ./solve_pt2.py inputfile")
+        sys.exit(2)
+    print(f"Part 2 solution: {solve(get_input(sys.argv[1]))}")
+
+
+if __name__ == "__main__":
+    main()
