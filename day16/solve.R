06f2a2c078661e06d7d0a6f6eaac0bc04d4ad7d8
Thomas M Ward <thomas@thomasward.com>
2020-12-16 13:28:38 -0500

    Add day 16 pt 1 solution in R
    CSVs lend themselves to treating the data as tabular. So use what I"m
    familiar with to do it. As a small caveat, I manually (well with vim and
    search replace) broke apart the input file into csvs.

---
+++ day16/solve.R
@@ -0,0 +1,46 @@
+#!/usr/bin/env Rscript
+
+library(tidyverse)
+
+rules <- read_csv("rules.csv")
+ticket_files <- c("your_ticket.csv", "nearby_tickets.csv")
+# ticket with value 1 is your ticket
+tickets <- map_dfr(ticket_files, read_csv, col_names = F) %>%
+    rowid_to_column(var = "ticket")
+
+valid_value <- function(val) {
+    total_valid <- rules %>%
+        rowwise() %>%
+        mutate(valid = between(val, min1, max1) || between(val, min2, max2)) %>%
+        ungroup() %>%
+        summarise(sum(valid)) %>%
+        pluck(1, 1)
+    total_valid > 0
+}
+
+# Pt 1 solution
+tickets %>%
+    pivot_longer(-ticket, names_to = "field", values_to = "value") %>%
+    mutate(valid = map_lgl(value, valid_value)) %>%
+    filter(!valid) %>%
+    summarise(pt_1_answer = sum(value))
+
+
+valid_values <- function(vals) {
+    is_valid  <- rep(FALSE, length(vals))
+    for (i in seq_along(vals)) {
+        for (r in seq(nrow(rules))){
+            if (between(vals[i], rules[[r, 2]], rules[[r, 3]]) && between(vals[i], rules[[r, 4]], rules[[r, 5]])) {
+                is_valid[i] = TRUE
+                break
+            }
+        }
+    }
+    is_valid
+}
+
+vals_to_test <- tickets %>%
+    pivot_longer(-ticket, names_to = "field", values_to = "value") %>%
+    pull(value)
+
+valid_values(vals_to_test)
